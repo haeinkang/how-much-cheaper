dd1e58385f27909b7bc61af00669864c2ecf5f46
hae._n <haein.kang7@gmail.com>
2025-02-10 12:50:06 +0900

    webdp 사진 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import {
   selectExchangeRates,
 } from "./features/exchange-slice";
 import { fetchProducts } from "./features/product-slice";
+import thumbnail0JPG from "./assets/images/thumbnails/thumbnail-0.jpg";
+import thumbnail2JPG from "./assets/images/thumbnails/thumbnail-2.jpg";
+import thumbnail3JPG from "./assets/images/thumbnails/thumbnail-3.jpg";
+import thumbnail0Webp from "./assets/images/thumbnails/_thumbnail-0.webp";
+import thumbnail2Webp from "./assets/images/thumbnails/_thumbnail-2.webp";
+import thumbnail3Webp from "./assets/images/thumbnails/_thumbnail-3.webp";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -15,7 +21,14 @@ function App() {
     dispatch(fetchProducts());
   }, []);
 
-  return <div className="body">{JSON.stringify(exchangeRates)}</div>;
+  return (
+    <div className="body">
+      <picture>
+        <source width={300} srcSet={thumbnail0Webp} type="image/webp" />
+        <img src={thumbnail0JPG} alt="" />
+      </picture>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/global.d.ts
@@ -0,0 +1,15 @@
+// src/global.d.ts
+declare module "*.webp" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
