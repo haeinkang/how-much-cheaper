4ec56f65871e308dda1ae60280c0372d4cef0ca4
hae._n <haein.kang7@gmail.com>
2025-02-15 15:36:43 +0900

    불필요 코드 제거

--- src/pages/ProductsPage/index.tsx
+++ src/pages/ProductsPage/index.tsx
@@ -13,24 +13,6 @@ import flatMap from "lodash/flatMap";
 import find from "lodash/find";
 import { selectTodayDealBasRByCurrency } from "../../features/exchange-slice";
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function ProductsPage() {
   const products = useSelector(selectProducts);
   const exchange = useSelector(selectTodayDealBasRByCurrency);
@@ -81,7 +63,4 @@ export default function ProductsPage() {
       </Table>
     </TableContainer>
   );
-}
-function selectTodayDeal_bas_rByCurrency(state: unknown): unknown {
-  throw new Error("Function not implemented.");
 }
